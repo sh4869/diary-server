1aab3195cbfecea5b6e05f1cea700af48f285479
sh4869 <nobuk4869@gmail.com>
2021-04-19 18:42:45 +0900

    できた

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use actix_files as fs;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_files as actix_fs;
+use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
+use std::env;
+use std::io::prelude::*;
+use std::path::Path;
+use std::process::Command;
+use std::{fs, fs::File};
 
 #[derive(Deserialize)]
 struct Diary {
@@ -9,9 +14,67 @@ struct Diary {
     date: String,
 }
 
+impl Diary {
+    fn to_file(&self) -> std::string::String {
+        format!("---\ntitle: {}\n---\n\n{}", self.title, self.content)
+    }
+}
+
 #[post("/diary")]
 async fn post_diary(req: web::Form<Diary>) -> impl Responder {
-    HttpResponse::Ok().body(&req.date)
+    let z = || {
+        let diary_repository_path = match env::var("DIARY_REPOSITORY_PATH") {
+            Ok(v) => v,
+            Err(e) => return Err(std::io::Error::new(std::io::ErrorKind::NotFound, e)),
+        };
+        // ファイルを作成
+        let dates: Vec<&str> = req.date.split("-").collect();
+        let path = format!(
+            "{}/{}/{}/{}.md",
+            diary_repository_path, dates[0], dates[1], dates[2]
+        );
+        let parent = Path::new(&path).parent().unwrap();
+        if !parent.exists() {
+            fs::create_dir_all(parent.to_str().unwrap())?;
+        }
+        let mut file = File::create(&path)?;
+        file.write_all(req.to_file().as_bytes())?;
+        // git pullの実行
+        let mut v = Command::new("git")
+            .args(&["pull", "origin", "HEAD"])
+            .current_dir(&diary_repository_path)
+            .spawn()?;
+        v.wait()?;
+        // git addの実行
+        let mut v = Command::new("git")
+            .args(&["add", "-A"])
+            .current_dir(&diary_repository_path)
+            .spawn()?;
+        v.wait()?;
+        // git commitの実行
+        let mut v = Command::new("git")
+            .args(&[
+                "commit",
+                "--all",
+                "-m",
+                format!("{}/{}/{} (from web)", dates[0], dates[1], dates[2]),
+            ])
+            .current_dir(&diary_repository_path)
+            .spawn()?;
+        v.wait()?;
+        // git pushの実行
+        let mut v = Command::new("git")
+            .args(&["push", "origin", "HEAD"])
+            .current_dir(&diary_repository_path)
+            .spawn()?;
+        v.wait()?;
+        Ok(true)
+    };
+
+    match z() {
+        Ok(_) => HttpResponse::Ok().body("updated!"),
+        Err(e) => HttpResponse::InternalServerError().body(format!("{}", e)),
+    }
 }
 
 #[actix_web::main]
@@ -19,7 +82,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(post_diary)
-            .service(fs::Files::new("/", "./static").index_file("index.html"))
+            .service(actix_fs::Files::new("/", "./static").index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
     .run()
