6882ffa09734bc9e08a9bc2cbc4e21700eb450ec
sh4869 <nobuk4869@gmail.com>
2021-04-21 23:59:10 +0900

    軽くリファクタリング

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_files as actix_fs;
 use actix_web::{middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
 use ansi_to_html::convert;
-use maud::{html, PreEscaped, DOCTYPE};
+use maud::{html, Markup, PreEscaped, DOCTYPE};
 use serde::Deserialize;
 use std::env;
 use std::io::prelude::*;
@@ -28,6 +28,36 @@ impl Diary {
     }
 }
 
+fn create_error_page(message: &str, stderr: &str) -> Markup {
+    html! {
+        (DOCTYPE)
+        html lang="ja" {
+            head {
+                meta charset="utf-8";
+                meta name="viewport" content="width=device-width, initial-scale=1";
+                style {(r##"
+                body {margin: auto; width: 1000px;}
+                pre {
+                    background-color: #eee;
+                    border-radius: 3px;
+                    border: 1px solid black;
+                    padding: 2px;
+                }
+                code {
+                    font-family: courier, monospace;
+                    padding: 0 3px;
+                }
+                "##)}
+            }
+            body {
+                h3 {(message)}
+                p {"error log"}
+                pre { code {(PreEscaped(convert(&stderr,false, false).unwrap()))} }
+            }
+        }
+    }
+}
+
 fn run_git_command(diary_repository_path: &str, args: &Vec<&str>) -> Result<(), Error> {
     let v = Command::new("git")
         .stdout(Stdio::piped())
@@ -35,79 +65,56 @@ fn run_git_command(diary_repository_path: &str, args: &Vec<&str>) -> Result<(), 
         .args(args)
         .current_dir(&diary_repository_path)
         .spawn()?;
+    let command_str = String::from("git ") + &args.join(" ");
+    log::debug!("run command : {}", command_str);
+
     let output = v.wait_with_output()?;
+    // エラー時の処理
     if !output.status.success() {
         let stderr = String::from_utf8(output.stderr).unwrap();
-        let joined = args.join(" ");
-        log::error!("failed on run command: git {}\n{}", joined, stderr);
-        let base_message = String::from("failed to run command: git ") + &joined;
-        let z = html! {
-            (DOCTYPE)
-            html lang="ja" {
-                head {
-                    meta charset="utf-8";
-                    meta name="viewport" content="width=device-width, initial-scale=1";
-                    style {(r##"body {margin: auto; width: 1000px;}
-                    pre {
-                        background-color: #eee;
-                        border-radius: 3px;
-                        border: 1px solid black;
-                        padding: 2px;
-                    }
-                    code {
-                        font-family: courier, monospace;
-                        padding: 0 3px;
-                    }
-                    "##)}
-                }
-                body {
-                    h3 {(base_message)}
-                    p {"error log"}
-                    pre { code {(PreEscaped(convert(&stderr,false, false).unwrap()))} }
-                }
-            }
-        };
+        log::error!("failed on run command: {}\n{}", command_str, stderr);
+        let z = create_error_page(&(String::from("failed to run command: ") + &command_str), &stderr);
         return Err(Error::new(ErrorKind::InvalidData, z.into_string()));
     }
-    log::info!("{}", String::from_utf8(output.stdout).unwrap());
+    log::debug!("{}", String::from_utf8(output.stdout).unwrap());
     Ok(())
 }
 
 fn v(req: web::Form<Diary>) -> Result<(), Error> {
+    // レポジトリパスの取得
     let diary_repository_path = match env::var("DIARY_REPOSITORY_PATH") {
         Ok(v) => v,
         Err(e) => return Err(std::io::Error::new(std::io::ErrorKind::NotFound, e)),
     };
+    // git pullの実行
+    run_git_command(&diary_repository_path, &Vec::from(["pull", "origin", "diary"]))?;
     // ファイルを作成
     let dates: Vec<&str> = req.date.split("-").collect();
     let path = format!("{}/{}/{}/{}.md", diary_repository_path, dates[0], dates[1], dates[2]);
-    let parent = Path::new(&path).parent().unwrap();
-    if !parent.exists() {
-        fs::create_dir_all(parent.to_str().unwrap())?;
+    {
+        let parent = Path::new(&path).parent().unwrap();
+        if !parent.exists() {
+            fs::create_dir_all(parent.to_str().unwrap())?;
+        }
+        let mut file = File::create(&path)?;
+        file.write_all(req.to_file().as_bytes())?;
     }
-    let mut file = File::create(&path)?;
-    file.write_all(req.to_file().as_bytes())?;
-    log::info!("WRITE FILE");
-    // git pullの実行
-    run_git_command(&diary_repository_path, &Vec::from(["pull", "origin", "diary"]))?;
-    log::info!("GIT PULL");
-    // git addの実行
+    // git add
     run_git_command(&diary_repository_path, &Vec::from(["add", "-A"]))?;
-    log::info!("GIT ADD");
-    // git commitの実行
-    let message = format!("{}/{}/{} (from web)", dates[0], dates[1], dates[2]);
-    run_git_command(&diary_repository_path, &Vec::from(["commit", "--all", "-m", &message]))?;
-    log::info!("GIT COMMIT");
-    // git pushの実行
+    // git commit
+    run_git_command(
+        &diary_repository_path,
+        &Vec::from(["commit", "--all", "-m", &format!("{}/{}/{} (from web)", dates[0], dates[1], dates[2])]),
+    )?;
+    // git push
     run_git_command(&diary_repository_path, &Vec::from(["push", "origin", "HEAD"]))?;
-    log::info!("GIT PUSH");
     Ok(())
 }
 
 #[post("/diary")]
 async fn post_diary(req: web::Form<Diary>) -> impl Responder {
     match v(req) {
-        Ok(_) => HttpResponse::Ok().body("updated!"),
+        Ok(_) => HttpResponse::Ok().body(html! {h1 {"updated!"}}.into_string()),
         Err(e) => HttpResponse::InternalServerError().body(format!("{}", e)),
     }
 }
